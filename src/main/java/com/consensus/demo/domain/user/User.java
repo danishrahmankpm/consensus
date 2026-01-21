9a119f0f4d824ab34f60674b9b0bec0bfe1c9eff
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2026-01-21 12:08:06 +0530

    chore:user entity implementation

--- src/main/java/com/consensus/demo/domain/user/User.java
+++ src/main/java/com/consensus/demo/domain/user/User.java
@@ -1,5 +1,35 @@
 package com.consensus.demo.domain.user;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    
+
+    public User(String email, String passwordHash, UserRole role) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
     
 }
+
---
+++ src/main/java/com/consensus/demo/domain/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.consensus.demo.domain.user;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
