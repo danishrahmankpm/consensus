9c986c407a2a9b2573c095d6c6ed681a5a01625b
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2026-01-22 01:49:19 +0530

    chore:MarketController implementation

--- src/main/java/com/consensus/demo/controller/MarketController.java
+++ src/main/java/com/consensus/demo/controller/MarketController.java
@@ -1,5 +1,25 @@
 package com.consensus.demo.controller;
 
+import org.springframework.stereotype.Controller;
+
+import com.consensus.demo.domain.market.MarketStatus;
+import com.consensus.demo.dto.request.CreateMarketRequest;
+import com.consensus.demo.dto.response.MarketResponse;
+import com.consensus.demo.service.MarketService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+@Controller
 public class MarketController {
+    @Autowired
+    MarketService marketService;
     
+
+    public ResponseEntity<MarketResponse> createMarket(CreateMarketRequest request) {
+        Long marketId = marketService.CreateMarket(request);
+       
+        MarketResponse response = new MarketResponse(marketId, MarketStatus.OPEN);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/consensus/demo/dto/response/MarketResponse.java
+++ src/main/java/com/consensus/demo/dto/response/MarketResponse.java
@@ -2,7 +2,12 @@ package com.consensus.demo.dto.response;
 
 import com.consensus.demo.domain.market.MarketStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
 public class MarketResponse {
     long marketId;
     MarketStatus status;
+   
 }
